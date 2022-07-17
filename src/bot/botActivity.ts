0195e073ca990e7f78319875b5b7c19ccf9ab925
hyperspace42 <kivs00@list.ru>
2022-07-18 00:39:45 +0300

    fix people count formatting

--- src/bot/botActivity.ts
+++ src/bot/botActivity.ts
@@ -1,6 +1,6 @@
 import { bot } from './index';
 
-import getServerStatus from '../utils/getServerStatus'
+import getServerStatus from '../utils/getServerStatus';
 
 import { IServerStatus } from 'types';
 
@@ -9,22 +9,23 @@ const formatActivityByPeopleCount = function (peopleCount: number): string {
 
   const lastDigit: number = +(String(peopleCount).at(-1) as string);
 
-  switch (lastDigit) {
-    case 0:
-      return 'майнкрафт один :с';
-    case 1:
-      return activityString.replace('%people', 'человеком');
-    default:
-      return activityString.replace('%people', 'людьми');
+  if (peopleCount === 0) {
+    return 'майнкрафт один :с';
   }
+
+  if (lastDigit === 1) {
+    return activityString.replace('%people', 'человеком');
+  }
+
+  return activityString.replace('%people', 'людьми');
 };
 
 export const setBotActivity = async function (): Promise<void> {
-  const serverStatus: IServerStatus = await getServerStatus()
+  const serverStatus: IServerStatus = await getServerStatus();
 
-  const playersOnline: number = serverStatus.players.online
+  const playersOnline: number = serverStatus.players.online;
 
   if (bot.user) {
-    bot.user.setActivity(formatActivityByPeopleCount(playersOnline), { type: "PLAYING" });
+    bot.user.setActivity(formatActivityByPeopleCount(playersOnline), { type: 'PLAYING' });
   }
 };
