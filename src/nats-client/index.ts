2b3ab495e5f4265120fec6c38b36923a454dc11b
BruhaBruh <drugsho.jaker@gmail.com>
2022-07-20 01:13:50 +0500

    add token feature to nats client

--- src/nats-client/index.ts
+++ src/nats-client/index.ts
@@ -1,35 +1,41 @@
-import nconf from 'nconf';
+import nconf from "nconf";
 
 nconf.file(`${process.cwd()}/config.json`);
 nconf.defaults({
-  NATS_SERVER_URL: '0.0.0.0:4222',
+  NATS_SERVER_URL: "0.0.0.0:4222",
 });
 
-import { INatsChatGlobalMessage } from '../types';
+import { INatsChatGlobalMessage } from "../types";
 
-import { connect, StringCodec, NatsConnection, Subscription } from 'nats';
+import { connect, NatsConnection, StringCodec, Subscription } from "nats";
 
-import { sendMessageEmbed } from '../bot/minecraftMessagesChannel';
+import { sendMessageEmbed } from "../bot/minecraftMessagesChannel";
 
-const NATS_SERVER_URL: string = nconf.get('NATS_SERVER_URL');
+const NATS_SERVER_URL: string = nconf.get("NATS_SERVER_URL");
+const NATS_TOKEN: string = nconf.get("NATS_TOKEN");
 
 const stringCodec = StringCodec();
 
 // #region functions
 
 const connectToNats = async function (): Promise<NatsConnection> {
-  const natsConnetion: Promise<NatsConnection> = connect({ servers: NATS_SERVER_URL });
+  const natsConnetion: Promise<NatsConnection> = connect({
+    servers: NATS_SERVER_URL,
+    token: NATS_TOKEN,
+  });
 
   console.log(`Connected to nats server ${NATS_SERVER_URL}`);
 
   return natsConnetion;
 };
 
-const subscribeToChatGlobal = async function (connectionPromise: Promise<NatsConnection>): Promise<Subscription> {
+const subscribeToChatGlobal = async function (
+  connectionPromise: Promise<NatsConnection>
+): Promise<Subscription> {
   const natsConnetion: NatsConnection = await connectionPromise;
-  const sub: Subscription = await natsConnetion.subscribe('chat.global');
+  const sub: Subscription = await natsConnetion.subscribe("chat.global");
 
-  console.log('Subscribed on chat.global');
+  console.log("Subscribed on chat.global");
 
   return sub;
 };
@@ -39,7 +45,8 @@ const subscribeToChatGlobal = async function (connectionPromise: Promise<NatsCon
 export const listenChatGlobal = async function (): Promise<void> {
   try {
     const natsConnetionPromise: Promise<NatsConnection> = connectToNats();
-    const subPromise: Promise<Subscription> = subscribeToChatGlobal(natsConnetionPromise);
+    const subPromise: Promise<Subscription> =
+      subscribeToChatGlobal(natsConnetionPromise);
 
     const sub = await subPromise;
 
